41808307f8fbfae9f97d773408e5e5753b9126fe
Ushi Philip <ushi7670741@gmail.com>
2026-02-04 22:24:35 +0200

    Sorting
    create a function that sort by alphabet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from menual_contacts import create_a_contact_file, show_manu, add_contact, show_all_contacts, search_contact, delete_contact, edit_contact
+from menual_contacts import create_a_contact_file, show_manu, add_contact, show_all_contacts, search_contact, delete_contact, edit_contact, sorting_by_alphabet
 
 print("Welcome To Our Contact Book!")
 create_a_contact_file()
@@ -7,7 +7,7 @@ while True:
     print(show_manu())
     choice = input("Please enter your choice: ")
 
-    if choice not in ["1","2","3","4","5","6"]:
+    if choice not in ["1", "2", "3", "4", "5", "6", "7"]:
         print("Please enter a valid number!")
         continue
 
@@ -36,6 +36,14 @@ while True:
         name = input("Enter a contact's name to edit: ")
         new_number = input("Enter a new number to edit: ")
         print(edit_contact(name, new_number))
+
+    elif choice == "6":
+        contacts = sorting_by_alphabet()
+        if type(contacts) == str:
+            print(contacts)
+        else:
+            for na, nu in contacts.items():
+                print(f"name : {na} - number : {nu}")
 
     else:
         break
