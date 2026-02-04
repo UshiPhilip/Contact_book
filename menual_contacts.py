5e4a15ebc0fd6e64f9be96de1d1fcc40023345e3
Ushi Philip <ushi7670741@gmail.com>
2026-02-04 20:56:06 +0200

    Add contact
    create a function to add a contact

--- menual_contacts.py
+++ menual_contacts.py
@@ -1,9 +1,10 @@
 from pathlib import Path
+import json
 
 def create_a_contact_file():
-    if not Path("contacts.txt").exists():
-        with open("contacts.txt", "w", encoding="utf-8") as f:
-            f.write("Welcome To Our Contacts book!\n\n")
+    if not Path("contacts.json").exists():
+        with open("contacts.json", "w", encoding="utf-8") as f:
+            json.dump({ }, f, indent="\t")
 
 
 def show_manu():
@@ -16,10 +17,19 @@ def show_manu():
 6. Exit
     """
 
-def show_contacts():
-    pass
-
 def add_contact(name, number):
+    with Path("contacts.json").open() as f:
+        contacts = json.load(f)
+    for n in contacts.keys():
+        if n == name:
+            return f"{name} is already exist."
+    contacts[name] = number
+
+    with Path("contacts.json").open("w") as f:
+        json.dump(contacts, f, indent="\t")
+    return f"{name} added successfully."
+
+def show_contacts():
     pass
 
 def save_contact(name, number):
