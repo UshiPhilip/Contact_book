75108914e1ac8cde4360375562f222cac1c11292
Ushi Philip <ushi7670741@gmail.com>
2026-02-04 20:35:46 +0200

    menu
    create the text file and the menu

--- menual_contacts.py
+++ menual_contacts.py
@@ -1,8 +1,20 @@
 from pathlib import Path
 
+def create_a_contact_file():
+    if not Path("contacts.txt").exists():
+        with open("contacts.txt", "w", encoding="utf-8") as f:
+            f.write("Welcome To Our Contacts book!\n\n")
+
 
 def show_manu():
-    pass
+    return """
+1. Add contact
+2. Show all contacts
+3. Search contact
+4. Delete contact
+5. Edit contact
+6. Exit
+    """
 
 def show_contacts():
     pass
