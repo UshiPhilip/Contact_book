ec53cd1cbb47a28fd0a622cc0579cd64db4f6646
Ushi Philip <ushi7670741@gmail.com>
2026-02-04 20:18:00 +0200

    Construction
    create a main file and a manager file

---
+++ menual_contacts.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+
+def show_manu():
+    pass
+
+def show_contacts():
+    pass
+
+def add_contact(name, number):
+    pass
+
+def save_contact(name, number):
+    pass
+
+def search_contact(name):
+    pass
+
+def delete_contact(name):
+    pass
+
+def edit_contact(old_name, new_name, new_number):
+    pass
